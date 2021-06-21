3ea0f0c45e6a87f22a6f8956dbd8940dbb28d011
Ishan <ishan.crdev@gmail.com>
2021-06-21 22:42:07 +0800

    setup the auth middleware to decode firebase tokens and authorize user

---
+++ packages/raffinato-backend/middleware/firebase.js
@@ -0,0 +1,13 @@
+const makeAuthAdmin = ({ auth }) => {
+  const verifyIdpToken = async ({ idpToken }) => {
+    console.log('\n\n',idpToken, '\n\n')
+    const decodedIdpToken = await auth.verifyIdToken(idpToken)
+    return { ...decodedIdpToken }
+  }
+
+  return Object.freeze({
+    verifyIdpToken
+  })
+}
+
+exports.makeAuthAdmin = makeAuthAdmin;
