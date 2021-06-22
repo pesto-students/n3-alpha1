8ad1ca877a343fc3292d7ca048e3db21194c19fc
Tara <tara@keepworks.com>
2021-06-22 11:06:15 +0530

    Add base API & react-query

--- packages/raffinato-frontend/src/index.tsx
+++ packages/raffinato-frontend/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import './index.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import AlertContainer from 'design-system/components/common/alert/AlertContainer';
 import { Navbar } from 'design-system';
 import { Home, Product, Shop } from 'pages';
@@ -10,32 +12,36 @@ import store from 'store/store';
 
 require('dotenv').config();
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <AlertContainer />
-      <Router>
-        {/* Global navbar */}
-        <Route path="/" exact>
-          <Navbar theme="light" />
-        </Route>
-        <Route path="/:other" exact>
-          <Navbar theme="dark" />
-        </Route>
-
-        {/* Page routes */}
-        <Switch>
+      <QueryClientProvider client={queryClient}>
+        <AlertContainer />
+        <Router>
+          {/* Global navbar */}
           <Route path="/" exact>
-            <Home />
+            <Navbar theme="light" />
           </Route>
-          <Route path="/shop">
-            <Shop />
+          <Route path="/:other" exact>
+            <Navbar theme="dark" />
           </Route>
-          <Route path="/product/:id">
-            <Product />
-          </Route>
-        </Switch>
-      </Router>
+
+          {/* Page routes */}
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/shop">
+              <Shop />
+            </Route>
+            <Route path="/product/:id">
+              <Product />
+            </Route>
+          </Switch>
+        </Router>
+      </QueryClientProvider>
     </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
