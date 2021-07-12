593f6a15465bc0deb8ea7167b2447a03e0d94c63
Tara <tara@keepworks.com>
2021-07-12 17:15:24 +0530

    Sync Cart - backend

--- packages/raffinato-backend/controllers/cartController.js
+++ packages/raffinato-backend/controllers/cartController.js
@@ -2,7 +2,7 @@ const { v4: uuid } = require('uuid');
 
 exports.addToCart = async (req, res) => {
   const { auth } = res.locals;
-  const item = req.body;
+  const { item } = req.body;
 
   const cartItem = {
     ...item,
@@ -10,10 +10,51 @@ exports.addToCart = async (req, res) => {
     userId: auth.uid,
   };
 
-  global.cartCollection.push(cartItem);
-
   res.json({
     status: 200,
     ...cartItem,
+  });
+};
+
+exports.removeFromCart = async (req, res) => {
+  const { auth } = res.locals;
+  const item = req.body;
+
+  global.cartCollection = global.cartCollection.filter((cartItem) => (
+    cartItem.id === item.id
+    && cartItem.userId === auth.uid
+  ));
+
+  res.json({
+    status: 200,
+    item,
+  });
+};
+
+exports.getCart = async (req, res) => {
+  const { auth } = res.locals;
+
+  const cartByUser = global.cartCollection.filter((item) => item.userId === auth.uid);
+
+  res.json({
+    status: 200,
+    items: cartByUser,
+  });
+};
+
+exports.syncCart = async (req, res) => {
+  const { auth } = res.locals;
+  const { items } = req.body;
+
+  const itemsWithUserId = items.map((item) => ({
+    ...item,
+    id: uuid(),
+    userId: auth.uid,
+  })) || [];
+
+  global.cartCollection.push(...itemsWithUserId);
+
+  res.json({
+    status: 200,
   });
 };
